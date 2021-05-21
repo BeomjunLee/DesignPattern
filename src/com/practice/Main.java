444798fb55dfe6c4634bfa15e316ad85c4908960
leebeomjun <qjawnswkd0717@gmail.com>
2021-05-21 14:59:41 +0900

    팩토리 메소드

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -1,7 +1,11 @@
 package com.practice;
 
+import com.practice.templateMethodPattern.DefaultGameConnectHelper;
+
 public class Main {
 
     public static void main(String[] args) {
+        DefaultGameConnectHelper defaultGameConnectHelper = new DefaultGameConnectHelper();
+        defaultGameConnectHelper.requestConnection("abc");
     }
 }
